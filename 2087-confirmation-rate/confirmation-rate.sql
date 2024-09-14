40ac6172cc209fe700d22ebea9952669aacc942e
Lokesh Bottu <82814830+lokesh-bottu@users.noreply.github.com>
2024-09-14 19:20:04 +0530

    Delete 2087-confirmation-rate directory

--- 2087-confirmation-rate/confirmation-rate.sql
+++
@@ -1,4 +0,0 @@
-# Write your MySQL query statement below
-SELECT s.USER_ID,ifnull(round(sum(case when action = 'confirmed' then 1 else 0 end)/count(action),2),0) confirmation_rate from Signups s
-left join Confirmations c on s.user_id = c.user_id
-group by s.user_id;
